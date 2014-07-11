0b30f221d70c37bcb17c20df86fb525c0f03665c
erwin.bautista <erwin.bautista@isacorp.tfs.local>
2014-07-11 18:00:03 +0000

    initial setup for WO HTTP for EAM
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C1277

---
+++ MOM Brands - EAM integration/WO_HTTP_SOAP/WO_HTTP_SOAP/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WO_HTTP_SOAP")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("SDI")> 
+<Assembly: AssemblyProduct("WO_HTTP_SOAP.EXE")> 
+<Assembly: AssemblyCopyright("Copyright © SDI 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("97051d0e-b6c7-44b3-a578-1c90bebc64eb")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
