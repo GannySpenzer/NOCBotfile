6009f6e083a80f4b8f6cf5a58d5b16a173410917
erwin.bautista <erwin.bautista@isacorp.tfs.local>
2014-10-22 17:13:50 +0000

    fix order by to properly recognized correct itemID/commodity code for a given part#
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C1825

--- SDI.CPJunctionSync/SDI.CPJunctionSync/GetClassAvailableProductItemsForProdView.sql
+++ SDI.CPJunctionSync/SDI.CPJunctionSync/GetClassAvailableProductItemsForProdView.sql
@@ -7,4 +7,4 @@
 from [{0}].dbo.ClassAvailableProducts cap
 where cap.catalogID = {1}
   and cap.productViewID = {2}
-order by cap.customerItemID, cap.updDate ASC
+order by cap.customerItemID, isnull(cap.updDate,cap.crDate) ASC
