b020e1a9beba0bd9b2c42e453dfdd7c1a3409942
erwin.bautista <erwin.bautista@isacorp.tfs.local>
2014-12-15 18:32:03 +0000

    initial setup for PO/WO process
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C1961

---
+++ MOM Brands - EAM integration/POWO_HTTP_SOAP/POWO_HTTP_SOAP/POWO_HTTP_SOAP/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SDI POWO_HTTP_SOAP")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("SDI-IMAGE")> 
+<Assembly: AssemblyProduct("SID.POWO_HTTP_SOAP")> 
+<Assembly: AssemblyCopyright("Copyright © SDI-IMAGE 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e419c153-f405-46cb-8c0a-b58e27e1b4dd")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
