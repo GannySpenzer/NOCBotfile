18ffb0fc444bff26370b36fc6c5b7cd6df94e5f2
eva.bard <eva.bard@isacorp.tfs.local>
2014-12-17 21:38:42 +0000

    1-Added SDI namespace to RCTP_PRICE program. 2-Changed PromptCode ID per Sam
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C1984

--- MOM Brands - EAM integration/RCPT_PRICE_HTTP_SOAP/RCPT_PRICE_HTTP_SOAP/RCPT_PRICE_HTTP_SOAP/My Project/Settings.Designer.vb
+++ MOM Brands - EAM integration/RCPT_PRICE_HTTP_SOAP/RCPT_PRICE_HTTP_SOAP/RCPT_PRICE_HTTP_SOAP/My Project/Settings.Designer.vb
@@ -192,9 +192,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.RCPT_PRICE_HTTP_SOAP.My.MySettings
+        Friend ReadOnly Property Settings() As Global.SDI.RCPT_PRICE_HTTP_SOAP.My.MySettings
             Get
-                Return Global.RCPT_PRICE_HTTP_SOAP.My.MySettings.Default
+                Return Global.SDI.RCPT_PRICE_HTTP_SOAP.My.MySettings.Default
             End Get
         End Property
     End Module
