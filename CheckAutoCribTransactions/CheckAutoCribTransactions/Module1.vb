36a2a8b0960ce7b40789047300b309ed06bf9321
vitaly.rovensky <vitaly.rovensky@isacorp.tfs.local>
2015-03-19 22:28:51 +0000

    VR 03/19/2015 New Changes set # 1
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C2389

--- CheckAutoCribTransactions/CheckAutoCribTransactions/Module1.vb
+++ CheckAutoCribTransactions/CheckAutoCribTransactions/Module1.vb
@@ -85,8 +85,137 @@ Module Module1
                     New OleDbDataAdapter(Command)
         Dim ds As System.Data.DataSet = New System.Data.DataSet
 
+        Try
+            dataAdapter.Fill(ds)
+            connectOR.Close()
+        Catch ex As Exception
+            objStreamWriter.WriteLine("     error selecting from ps_isa_enterprise table")
+            objStreamWriter.WriteLine("         " & ex.Message)
+            connectOR.Close()
+            Return True
+        End Try
+
+        If ds.Tables(0).Rows.Count = 0 Then
+            objStreamWriter.WriteLine(" 0 rows selected from ps_isa_enterprise table")
+            connectOR.Close()
+            Return False
+        Else
+            'check how many trans for each selected BU
+            Dim I As Integer
+
+            objStreamWriter.WriteLine(ds.Tables(0).Rows.Count & "  rows selected from ps_isa_enterprise table")
+            Try
+                connectOR.Open()
+            Catch ex As Exception
+                objStreamWriter.WriteLine(" Error on connectOR.Open() ")
+            End Try
+            For I = 0 To ds.Tables(0).Rows.Count - 1
+                'objStreamWriter.WriteLine(" Going to check row: " & I.ToString())
+                CheckAndSendCustEmail(ds.Tables(0).Rows(I))
+            Next
+        End If
+
         Return bResult
 
+    End Function
+
+    Private Function CheckAndSendCustEmail(ByVal dr As DataRow) As Boolean
+        Dim bReslt As Boolean = False
+        '  ISA_COMPANY_ID " & dr.Item("ISA_COMPANY_ID") & "
+
+        Dim strSQLstring As String
+        'Dim strFirstName As String
+        'Dim strLastName As String
+
+        strSQLstring = "select * from SYSADM.ps_isa_autocrb_trx " & vbCrLf & _
+                    " where business_unit='" & dr.Item("isa_business_unit") & "' " & vbCrLf & _
+                    " and trunc(dt_timestamp, 'DDD') > trunc(sysdate - 2, 'DDD')"
+
+        Try
+            objStreamWriter.WriteLine(" strSQLstring: " & strSQLstring)
+            Dim Command1 As OleDbCommand = New OleDbCommand(strSQLstring, connectOR)
+
+            Dim dataAdapter1 As OleDbDataAdapter = _
+                        New OleDbDataAdapter(Command1)
+            Dim ds1 As System.Data.DataSet = New System.Data.DataSet
+            objStreamWriter.WriteLine("Place1")
+            dataAdapter1.Fill(ds1)
+            Try
+                objStreamWriter.WriteLine("Place2 " & ds1.Tables.Count)
+            Catch ex As Exception
+                objStreamWriter.WriteLine("Error: Place2")
+            End Try
+            Try
+                objStreamWriter.WriteLine("Place2-A " & ds1.Tables(0).Rows.Count)
+            Catch ex As Exception
+                objStreamWriter.WriteLine("Error: Place2-A")
+            End Try
+            If Not ds1 Is Nothing Then
+                If ds1.Tables.Count > 0 Then
+                    If ds1.Tables(0).Rows.Count = 0 Then
+                        'send e-mail
+
+                        Dim Mailer As MailMessage = New MailMessage
+                        objStreamWriter.WriteLine("Place3")
+
+                        With Mailer
+
+                            .From = "TechSupport@sdi.com"
+                            .To = "vitaly.rovensky@sdi.com"
+                            If connectOR.DataSource.ToUpper = "RPTG" Or _
+                                    connectOR.DataSource.ToUpper = "SNBX" Or _
+                                    connectOR.DataSource.ToUpper = "DEVL" Then
+
+                                .To = "DoNotSendRPTG@sdi.com"
+                            End If
+
+                            Dim dDate As Date = CDate(Now())
+                            dDate = dDate.AddDays(-1)
+
+                            .Subject = "No Autocrib Transaction for " & dr.Item("ISA_COMPANY_ID") & " (BU: " & dr.Item("isa_business_unit") & ") on " & dDate.ToShortDateString()
+                            .Priority = MailPriority.High
+                            .BodyFormat = MailFormat.Html
+                            .Body = "<html><body><table><tr><td>No transactions processed for site " & dr.Item("ISA_COMPANY_ID") & " (BU: " & dr.Item("isa_business_unit") & ") via autocrib web services on " & dDate.ToShortDateString() & "</td></tr>"
+
+
+                        End With
+                        objStreamWriter.WriteLine("Place4")
+
+                        bReslt = sendemail(Mailer)
+                        objStreamWriter.WriteLine("Place5")
+                        If bReslt = False Then
+                            objStreamWriter.WriteLine("  Error sending email to: " & Mailer.To & ", from: " & Mailer.From & ", - at " & Now().ToString())
+                        Else
+                            objStreamWriter.WriteLine("E-mail was sent successfully for " & dr.Item("ISA_COMPANY_ID") & " BU: " & dr.Item("isa_business_unit") & "")
+                        End If
+                    End If
+                Else
+                    objStreamWriter.WriteLine("No data from SYSADM.ps_isa_autocrb_trx for " & dr.Item("ISA_COMPANY_ID") & " BU: " & dr.Item("isa_business_unit") & "")
+                End If
+            Else
+                objStreamWriter.WriteLine("No data from SYSADM.ps_isa_autocrb_trx for " & dr.Item("ISA_COMPANY_ID") & " BU: " & dr.Item("isa_business_unit") & "")
+            End If
+        Catch ex As Exception
+            bReslt = False
+            objStreamWriter.WriteLine("Error retrieving data from SYSADM.ps_isa_autocrb_trx for " & dr.Item("ISA_COMPANY_ID") & " BU: " & dr.Item("isa_business_unit") & "")
+            objStreamWriter.WriteLine("Error: " & ex.Message)
+        End Try
+
+        Return bReslt
+    End Function
+
+    Private Function sendemail(ByVal mailer As MailMessage) As Boolean
+        Dim bNoError As Boolean = False
+        Try
+            'SmtpMail.Send(mailer)
+
+            UpdEmailOut.UpdEmailOut.UpdEmailOut(mailer.Subject, mailer.From, mailer.To, "", "", "N", mailer.Body, connectOR)
+
+            bNoError = True
+        Catch ex As Exception
+            bNoError = False
+        End Try
+        Return bNoError
     End Function
 
     Private Sub SendErrEmail()
