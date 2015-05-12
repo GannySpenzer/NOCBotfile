6cea2d5d145a39c96d0f943243455c7c9fa9e9b0
erwin.bautista <erwin.bautista@isacorp.tfs.local>
2015-05-12 15:04:03 +0000

    modified query to look at ProductViews table instead of ClassAvailableProducts for lists of catalog + product view IDs since there were newly added sites/product views (ie., I0945 pv#290) that don't have any entry in ClassAvailableProducts BUT wants their P/S items to be searchable - Erwin (and the craze starts :D)
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C2537

--- ItemCatalogAdd/SDI.StagedCatalogItem/SDI.StagedCatalogItem/GetCatalogProductView.sql
+++ ItemCatalogAdd/SDI.StagedCatalogItem/SDI.StagedCatalogItem/GetCatalogProductView.sql
@@ -1,6 +1,6 @@
 ﻿select 
-  A.catalogId as CATALOG_ID
+  A.classId as CATALOG_ID
 , A.productViewId as PRODUCT_VIEW_ID
-from classAvailableProducts A
-group by A.catalogId, A.productViewId 
-order by A.catalogId, A.productViewId 
+from productViews A
+group by A.classId, A.productViewId 
+order by A.classId, A.productViewId 
