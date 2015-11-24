7aceae733ebf41588225a7202378792a6af62529
eva.bard <eva.bard@isacorp.tfs.local>
2015-11-24 14:53:01 +0000

    Check in files that were excluded from source control (for some reason!)
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C3104

---
+++ EmailToTicket/EmailToTicket/EmailToTicket/EmailOpsStaff/EmailOpsStaff/EmailOpsStaff/EmailOpsStaff.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Xml
+Imports System.IO
+Imports System.Data.OleDb
+Imports System.Web.Mail
+Imports System.Web.UI
+Imports System.Text
+
+Module EmailOpsStaff
+    Public ClsLogger As LoggerClass
+    Dim connectOR As New OleDbConnection("Provider=MSDAORA.1;Password=EINTERNET;User ID=EINTERNET;Data Source=prod")
+
+
+    Sub Main()
+        If Command() = "Setup" Then
+            'If Command() = "" Then
+
+            '  Dim myExchange As New CytecRCTClass
+            Dim frmSetup As New SetupForm
+
+            frmSetup.ShowDialog()
+
+        Else
+            Try
+
+
+
+                Dim runControl As runControl
+                Dim currentRunControlIndex1 As Integer
+                Dim currentRunControlIndex As Integer
+                Dim runFolder As String
+                Dim processor As ProcessorClass
+                Dim currentRunTime As DateTime
+                runFolder = Environment.CurrentDirectory
+
+                ClsLogger = New LoggerClass(runFolder & "\Logs")
+                ClsLogger.Open_Log_file("ExchangeAttachProcessor")
+                runFolder &= "\RunControl.xml"
+
+
+                'Dim myExchange As New PersonalEmailClass
+                'myExchange.Main()
+                runControl = runControl.Load(runFolder, GetType(runControl))
+
+                If runControl.Items.Count > 0 Then
+
+                    For Each runInfo As runInformation In runControl.Items
+
+                        If runInfo.Enabled Then
+                            currentRunTime = Now
+                            ClsLogger.Log_Event("Processing RunName: " & runInfo.RunName & " at " & currentRunTime.ToString)
+
+                            processor = New ProcessorClass(runInfo, ClsLogger)
+                            Try
+                                processor.oraCNString = CStr(My.Settings("oraCNString")).Trim
+                            Catch ex As Exception
+                            End Try
+                            Try
+                                processor.sqlCNString = CStr(My.Settings("sqlCNString")).Trim
+                            Catch ex As Exception
+                            End Try
+                            ClsLogger.Log_Event("After process class instantiation")
+                            If processor.Main(currentRunTime) Then
+                                ClsLogger.Log_Event("Processor.Main returned True")
+                                'msgbox("after Processor.Main() = true")
+                                runInfo.LastRunTime = currentRunTime
+                            Else
+                                ClsLogger.Log_Event("Processor.Main returned False")
+                            End If
+
+                        Else
+                            ClsLogger.Log_Event(runInfo.RunName & " Is not enabled")
+                        End If
+                    Next
+                    runControl.Save(runFolder)
+                End If
+            Catch ex As Exception
+                ClsLogger.Log_Event("START UP MAIN ERROR: " & ex.Message)
+            End Try
+            ClsLogger.Close_Log_file()
+            ClsLogger = Nothing
+        End If
+    End Sub
+
+End Module
---
+++ EmailToTicket/EmailToTicket/EmailToTicket/EmailOpsStaff/EmailOpsStaff/EmailOpsStaff/ProcessorClass.vb
@@ -0,0 +1,55 @@
+﻿Public Class ProcessorClass
+    Private m_RunInfo As runInformation
+    Private m_logger As LoggerClass
+
+    Public Sub New(ByVal runInfo As runInformation, ByVal Logger As LoggerClass)
+        m_RunInfo = runInfo
+        m_logger = Logger
+    End Sub
+
+    Public Function Main(Optional ByVal dttmNow As DateTime = Nothing) As Boolean
+        Dim bReturn As Boolean = False
+        'msgbox("entering processor main")
+        m_logger.Log_Event("Processor Class Main")
+        Select Case m_RunInfo.RunName
+            'Case Is = "CytecRCT"
+
+            Case Else
+                m_logger.Log_Event("entering processor main case else ")
+                Dim RunClass As New GenericClass(m_RunInfo)
+				if (m_oraCNString.Trim.Length > 0) Then
+					RunClass.SetORACN(m_oraCNString.Trim)
+				end if
+				if (m_sqlCNString.Trim.Length > 0) Then
+					RunClass.SetSQLCN(m_sqlCNString.Trim)
+				end if
+                RunClass.clsLogger = m_logger
+
+                bReturn = RunClass.ProcessMailbox(dttmNow)
+                RunClass = Nothing
+
+        End Select
+        Return bReturn
+
+    End Function
+	
+	private m_oraCNString as string = ""
+	public property oraCNString as string
+        Get
+            Return m_oraCNString
+        End Get
+        Set(ByVal Value As String)
+            m_oraCNString = Value
+        End Set
+	end property 
+	
+	private m_sqlCNString as string = ""
+	public property sqlCNString as string
+        Get
+            Return m_sqlCNString
+        End Get
+        Set(ByVal Value As String)
+            m_sqlCNString = Value
+        End Set
+	end property
+End Class
