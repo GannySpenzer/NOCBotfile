815d53c3b1e2e334c2ca88ba6cff8674044b5ebd
AVACORP <AVACORP@sdi.com>
2017-08-10 09:45:12 +0000

    Changed datasource to STAR
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C5110

--- EmailToTicket/EmailOpsStaff/EmailOpsStaff/EmailOpsStaff/GenericClass.vb
+++ EmailToTicket/EmailOpsStaff/EmailOpsStaff/EmailOpsStaff/GenericClass.vb
@@ -15,7 +15,7 @@ Imports UpdEmailOut
  
 Public Class GenericClass
     Inherits ExchangeBaseClass
-    Dim connectOR As New OleDbConnection("Provider=MSDAORA.1;Password=einternet;User ID=einternet;Data Source=prod")
+    Dim connectOR As New OleDbConnection("Provider=MSDAORA.1;Password=einternet;User ID=einternet;Data Source=STAR")
     Dim connectSQL As New SqlClient.SqlConnection("server=192.168.253.16;uid=sa;pwd=sdiadmin;initial catalog='pubs'")
     Dim objStreamWriter As StreamWriter
     'server=192.168.253.16;uid=sa;pwd=sdiadmin;initial catalog=pubs;
