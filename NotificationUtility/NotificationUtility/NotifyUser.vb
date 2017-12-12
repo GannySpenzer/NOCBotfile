87779f9919154e2b8fa800d7bdab6c8a4eb412c6
AVACORP <AVACORP@sdi.com>
2017-12-12 15:59:32 +0000

    Added email utility services web reference and added appsetting to maintain the notification type
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C5842

--- NotificationUtility/NotificationUtility/NotifyUser.vb
+++ NotificationUtility/NotificationUtility/NotifyUser.vb
@@ -12,11 +12,45 @@ Public Class Notifications
     Public Shared Sub AddToNotifyQueue()
         Try
             Dim StrQry As String = "INSERT INTO SDIX_NOTIFY_QUEUE (NOTIFY_ID,NOTIFY_TYPE,USER_ID,DTTMADDED,TITLE,LINK,HTMLMSG,STATUS,ATTACHMENTS)  " & vbCrLf & _
-                                   "SELECT NOTIFY_QUEUE_SEQ.NEXTVAL,'Anoun',OPRIDADDED,TO_DATE('" & Now() & "', 'MM/DD/YYYY HH:MI:SS AM'), " & vbCrLf & _
+                                   "SELECT NOTIFY_QUEUE_SEQ.NEXTVAL,'" & ConfigurationSettings.AppSettings("NotificationType") & "',OPRIDADDED,TO_DATE('" & Now() & "', 'MM/DD/YYYY HH:MI:SS AM'), " & vbCrLf & _
                                    "(CASE ACTION WHEN 'FINAL' THEN 'Finalization event was completed at ' || To_char(FINALDTTM,'MM/DD/YYYY HH:MI:SS AM')   " & vbCrLf & _
                                    "ELSE  'Reconciliation event was completed at ' || To_char(FINALDTTM,'MM/DD/YYYY HH:MI:SS AM') END) As Title,' ',' ','N',' '  FROM SDIX_CYCLE_COUNT_FINAL_LOG WHERE FINALDTTM BETWEEN TO_DATE('" & Now().AddMinutes(Convert.ToInt32(-ConfigurationSettings.AppSettings("IntervalTime"))) & "','MM/DD/YYYY HH:MI:SS AM') AND TO_DATE('" & Now() & "','MM/DD/YYYY HH:MI:SS AM') "
             Dim rowAffected As Integer = ORDBData.ExecNonQuery(StrQry)
         Catch ex As Exception
+            Dim sMyErrorString As String = String.Empty
+            If Not ex Is Nothing Then
+                sMyErrorString = ex.ToString
+            End If
+            sMyErrorString += sMyErrorString & _
+                       "Error in NotificationUtility -> AddToNotifyQueue"
+
+            SendSDiExchErrorMail(sMyErrorString)
+        End Try
+    End Sub
+    Public Shared Sub SendSDiExchErrorMail(ByVal strErrorMessage As String, Optional ByVal sSubject As String = "")
+        Dim strComeFrom As String = "Unknown"
+        Try
+            strComeFrom = ConfigurationSettings.AppSettings("serverId").Trim
+        Catch ex As Exception
+            strComeFrom = "Unknown"
+        End Try
+        Dim mail As New System.Net.Mail.MailMessage()
+        mail.To.Add("webdev@sdi.com")
+        mail.To.Add("sdiportalsupport@avasoft.biz")
+        mail.From = New System.Net.Mail.MailAddress("SDIExchADMIN@sdi.com", "SDiExchange Admin")
+        ' adding server name to the error message
+        mail.Body = "Error in SDiExchange  -  " & vbCrLf & strErrorMessage & ". Server Id: " & strComeFrom
+        If sSubject.Trim.Length = 0 Then
+            sSubject = "Error - SDiExchange"
+        End If
+        mail.Subject = sSubject
+        Dim SDIEmailService As SDiEmailUtilityService.EmailServices = New SDiEmailUtilityService.EmailServices()
+        Dim MailAttachmentName As String()
+        Dim MailAttachmentbytes As New List(Of Byte())()
+        Try
+            SDIEmailService.EmailUtilityServices("MailandStore", mail.From.ToString(), mail.To.ToString(), mail.Subject, String.Empty, String.Empty, mail.Body, "SDIERRMAIL", MailAttachmentName, MailAttachmentbytes.ToArray())
+        Catch ex As Exception
+            Dim strErr As String = ex.Message
         End Try
     End Sub
 End Class
