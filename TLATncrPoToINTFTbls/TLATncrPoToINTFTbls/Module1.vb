9c15c8db4e2726be91eb920e563777ae8a683293
vitaly.rovensky <vitaly.rovensky@isacorp.tfs.local>
2018-01-10 21:26:18 +0000

    VR Initial Project check in
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C5910

---
+++ TLATncrPoToINTFTbls/TLATncrPoToINTFTbls/Module1.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Data
+Imports System.IO
+Imports System.Data.OleDb
+Imports System.Web
+'Imports System.Web.Mail
+'Imports System.Web.UI
+Imports System.Xml
+Imports System.Text
+Imports System.Net
+Imports System.Net.Mail
+Imports SDI.ApplicationLogger
+Imports SDI.UNCC.WorkOrderAdapter
+
+Module Module1
+
+    Private m_logger As appLogger = Nothing
+    Private myLoggr1 As appLogger = Nothing
+
+    Dim rootDir As String = "C:\CytecMxmIn"
+    Dim logpath As String = "C:\CytecMxmIn\LOGS\UpdMatMastCytecXmlIn" & Now.Year & Now.Month & Now.Day & Now.GetHashCode & ".txt"
+    Dim sErrLogPath As String = "C:\CytecMxmIn\LOGS\MyErredSQLs" & Now.Year & Now.Month & Now.Day & Now.GetHashCode & ".txt"
+    Dim connectOR As New OleDbConnection("Provider=OraOLEDB.Oracle.1;Password=sd1exchange;User ID=sdiexchange;Data Source=DEVL")
+    Dim connectSQL As New SqlClient.SqlConnection("server=cplus_prod;uid=einternet;pwd=einternet;initial catalog='contentplus'")
+    Dim strOverride As String
+    Dim bolWarning As Boolean = False
+
+    Private m_arrXMLErrFiles As String
+    Private m_arrErrorsList As String
+
+    Sub Main()
+
+    End Sub
+
+End Module
