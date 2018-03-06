822c3fcd4bf7d25ef82e299815c8dafc3927014a
vitaly.rovensky <vitaly.rovensky@isacorp.tfs.local>
2018-03-06 20:26:52 +0000

    VR - Amazon Direct Send orders to AMAZON - initial check in
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C6103

---
+++ AmazonDirectSend/AmazonDirectSend/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Net.Security
+Imports System.Security.Cryptography.X509Certificates
+Imports System.Web
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Xml
+
+Module Module1
+
+    'WARNING!!! This project is read Oracle for AMAZON orders and send them to Amazon as cXML file - VR 03/06/2018
+
+    Sub Main()
+
+    End Sub
+
+End Module
