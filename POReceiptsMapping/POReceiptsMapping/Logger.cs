73d4ce86f96087f2f22e5d0098283fc168471596
240916Yury Arkadin <Yury.Arkadin@sdi.com>
2018-12-26 20:14:58 +0000

    updating email distro list for errors
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C7226

--- POReceiptsMapping/POReceiptsMapping/Logger.cs
+++ POReceiptsMapping/POReceiptsMapping/Logger.cs
@@ -119,7 +119,8 @@ namespace POMapping
                 strBody = strBodyhead + strbodydetl;
                 try
                 {
-                    SDIEmailService.EmailUtilityServices("Mail", "sdiportalsupport@avasoft.biz","karthick.k.s@avasoft.com", "Error from POReceiptMapping Utility", "", "", strBody, "SDIERRMAIL", MailAttachmentName, MailAttachmentbytes.ToArray());
+                    //SDIEmailService.EmailUtilityServices("Mail", "sdiportalsupport@avasoft.biz","karthick.k.s@avasoft.com", "Error from POReceiptMapping Utility", "", "", strBody, "SDIERRMAIL", MailAttachmentName, MailAttachmentbytes.ToArray());
+                    SDIEmailService.EmailUtilityServices("Mail", "SDIExchADMIN@sdi.com", "webdev@sdi.com; Donna.Ciampoli@sdi.com; scott.doyle@sdi.com", "Error from POIssueMapping Utility", "", "", strBody, "SDIERRMAIL", MailAttachmentName, MailAttachmentbytes.ToArray());
                 }
                 catch (Exception ex1)
                 {
