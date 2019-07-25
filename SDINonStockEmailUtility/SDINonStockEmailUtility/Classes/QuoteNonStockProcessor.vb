b0372caa88ee84777c5cfd7d33de5c27cd1c9990
vitaly.rovensky <vitaly.rovensky@isacorp.tfs.local>
2019-07-25 12:48:23 +0000

    VR Changes for sending email to every Employee ID in the order
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C8197

--- SDINonStockEmailUtility/SDINonStockEmailUtility/Classes/QuoteNonStockProcessor.vb
+++ SDINonStockEmailUtility/SDINonStockEmailUtility/Classes/QuoteNonStockProcessor.vb
@@ -225,9 +225,9 @@ Public Class QuoteNonStockProcessor
                   "FROM SYSADM8.PS_ISA_ORD_INTF_LN A " & vbCrLf & _
                   "WHERE NOT EXISTS (" & vbCrLf & _
                   "                  SELECT 'X' " & vbCrLf & _
-                  "                  FROM PS_ISA_REQ_EML_LOG B " & vbCrLf & _
+                  "                  FROM SDIX_REQ_EML_LOG B " & vbCrLf & _
                   "                  WHERE B.BUSINESS_UNIT = A.BUSINESS_UNIT_OM " & vbCrLf & _
-                  "                    AND B.REQ_ID = A.ORDER_NO " & vbCrLf & _
+                  "                    AND B.REQ_ID = A.ORDER_NO AND B.ISA_EMPLOYEE_ID = A.ISA_EMPLOYEE_ID " & vbCrLf & _
                   "                 ) " & vbCrLf & _
                   "  AND A.ISA_LINE_STATUS = 'QTS' " & vbCrLf & _
                   "  AND NOT EXISTS ( " & vbCrLf & _
@@ -822,9 +822,9 @@ Public Class QuoteNonStockProcessor
                                  "                 ) " & vbCrLf & _
                                  "  AND NOT EXISTS (" & vbCrLf & _
                                  "                  SELECT 'X'" & vbCrLf & _
-                                 "                  FROM PS_ISA_REQ_EML_LOG B1" & vbCrLf & _
+                                 "                  FROM SDIX_REQ_EML_LOG B1" & vbCrLf & _
                                  "                  WHERE B1.BUSINESS_UNIT = A.BUSINESS_UNIT" & vbCrLf & _
-                                 "                    AND B1.REQ_ID = A.REQ_ID" & vbCrLf & _
+                                 "                    AND B1.REQ_ID = A.REQ_ID AND B1.ISA_EMPLOYEE_ID = L.ISA_EMPLOYEE_ID " & vbCrLf & _
                                  "                 )" & vbCrLf & _
                                  " ORDER BY A1.BUSINESS_UNIT, A1.REQ_ID, A1.LINE_NBR " & vbCrLf & _
                                  ""
@@ -863,9 +863,13 @@ Public Class QuoteNonStockProcessor
                     bNew = True
                     boItem = Nothing
 
-                    ' get the key for the current record
+                    ' get the key for the current record - with Employee ID
                     cKey = CType(rdr("BUSINESS_UNIT"), String).Trim.PadLeft(5, CType(" ", Char)) & _
-                           CType(rdr("REQ_ID"), String).Trim.PadLeft(10, CType(" ", Char))
+                           CType(rdr("REQ_ID"), String).Trim.PadLeft(10, CType(" ", Char)) & _
+                           CType(rdr("ISA_EMPLOYEE_ID"), String).Trim.PadLeft(10, CType(" ", Char))
+
+                    'cKey = CType(rdr("BUSINESS_UNIT"), String).Trim.PadLeft(5, CType(" ", Char)) & _
+                    '       CType(rdr("REQ_ID"), String).Trim.PadLeft(10, CType(" ", Char))
 
                     ' check if current key exist or be in a new message instance
                     If m_colMsgs.Count > 0 Then
@@ -1773,8 +1777,8 @@ Public Class QuoteNonStockProcessor
 
                         ' build insert SQL command
                         cSQL = _
-                        "INSERT INTO PS_ISA_REQ_EML_LOG " & _
-                        "(BUSINESS_UNIT, REQ_ID, ISA_RECIPIENT, ISA_SENDER, ISA_SUBJECT, EMAIL_DATETIME) " & _
+                        "INSERT INTO SDIX_REQ_EML_LOG " & _
+                        "(BUSINESS_UNIT, REQ_ID, ISA_RECIPIENT, ISA_SENDER, ISA_SUBJECT, EMAIL_DATETIME, ISA_EMPLOYEE_ID) " & _
                         "VALUES " & _
                         "(" & _
                             "'" & CType(IIf(itmQuoted.BusinessUnitID.Length > 0, itmQuoted.BusinessUnitID, "."), String) & "', " & _
@@ -1782,7 +1786,7 @@ Public Class QuoteNonStockProcessor
                             "'" & "TO=" & eml.To & "CC=" & eml.Cc & "BCC=" & eml.Bcc & "', " & _
                             "'" & CType(IIf(eml.From.Length > 0, eml.From, "."), String) & "', " & _
                             "'" & CType(IIf(eml.Subject.Length > 0, eml.Subject, "."), String) & "', " & _
-                            "TO_DATE('" & System.DateTime.Now.ToString & "','MM/DD/YYYY HH:MI:SS AM') " & _
+                            "TO_DATE('" & System.DateTime.Now.ToString & "','MM/DD/YYYY HH:MI:SS AM'), '" & itmQuoted.EmployeeID & "' " & _
                         ")"
 
                         If m_CN.State = ConnectionState.Open Then
@@ -2013,7 +2017,7 @@ Public Class QuoteNonStockProcessor
                     ' build insert SQL command
                     cSQL = _
                     "INSERT INTO PS_ISA_REQ_EML_LOG " & _
-                    "(BUSINESS_UNIT, REQ_ID, ISA_RECIPIENT, ISA_SENDER, ISA_SUBJECT, EMAIL_DATETIME) " & _
+                    "(BUSINESS_UNIT, REQ_ID, ISA_RECIPIENT, ISA_SENDER, ISA_SUBJECT, EMAIL_DATETIME, ISA_EMPLOYEE_ID) " & _
                     "VALUES " & _
                     "(" & _
                         "'" & CType(IIf(itmQuoted.BusinessUnitID.Length > 0, itmQuoted.BusinessUnitID, "."), String) & "', " & _
@@ -2021,7 +2025,7 @@ Public Class QuoteNonStockProcessor
                         "'" & "TO=" & eml.To & "CC=" & eml.Cc & "BCC=" & eml.Bcc & "', " & _
                         "'" & CType(IIf(eml.From.Length > 0, eml.From, "."), String) & "', " & _
                         "'" & CType(IIf(eml.Subject.Length > 0, eml.Subject, "."), String) & "', " & _
-                        "TO_DATE('" & System.DateTime.Now.ToString & "','MM/DD/YYYY HH:MI:SS AM') " & _
+                        "TO_DATE('" & System.DateTime.Now.ToString & "','MM/DD/YYYY HH:MI:SS AM'), '" & itmQuoted.EmployeeID & "' " & _
                     ")"
 
                     If m_CN.State = ConnectionState.Open Then
@@ -2136,12 +2140,12 @@ Public Class QuoteNonStockProcessor
     End Function
 
     Private Function GetReqAltApprover(ByVal strUserid As String, ByVal strBU As String, ByVal Req_type As String) As String
+        Dim strReqApprEmailID As String = ""
         Try
             Dim strReqApprSet As String = "SELECT ISA_REQ_APR_ALT FROM SDIX_USERS_REQ_APPRV WHERE ISA_EMPLOYEE_ID = '" & strUserid & "' AND BUSINESS_UNIT = '" & strBU & "'"
             Dim ListOfReqApprSet As DataSet = ORDBData.GetAdapter(strReqApprSet)
             Dim getemailID_altAppr As DataSet
             'Dim listReqApprSet As New List(Of String)
-            Dim strReqApprEmailID As String = String.Empty
             If Not ListOfReqApprSet Is Nothing Then
                 If ListOfReqApprSet.Tables(0).Rows.Count = 1 Then
                     Dim strReqAltApprEmailID As String = "SELECT * FROM SDIX_USERS_TBL WHERE ISA_EMPLOYEE_ID = '" & Convert.ToString(ListOfReqApprSet.Tables(0).Rows(0).Item("ISA_REQ_APR_ALT")) & "'"
@@ -2165,10 +2169,11 @@ Public Class QuoteNonStockProcessor
             Else
                 strReqApprEmailID = ""
             End If
-            Return strReqApprEmailID
         Catch ex As Exception
-
+            strReqApprEmailID = ""
         End Try
+        Return strReqApprEmailID
+
     End Function
 
 
@@ -2620,8 +2625,8 @@ Public Class QuoteNonStockProcessor
                 End If
             End If
 
-            strInsertQuery = "INSERT INTO PS_ISA_REQ_EML_LOG " & _
-                "(BUSINESS_UNIT, REQ_ID, ISA_RECIPIENT, ISA_SENDER, ISA_SUBJECT, EMAIL_DATETIME) " & _
+            strInsertQuery = "INSERT INTO SDIX_REQ_EML_LOG " & _
+                "(BUSINESS_UNIT, REQ_ID, ISA_RECIPIENT, ISA_SENDER, ISA_SUBJECT, EMAIL_DATETIME, ISA_EMPLOYEE_ID) " & _
                 "VALUES " & _
                 "(" & _
                     "'" & CType(IIf(itmQuoted.BusinessUnitID.Length > 0, itmQuoted.BusinessUnitID, "."), String) & "', " & _
@@ -2629,7 +2634,7 @@ Public Class QuoteNonStockProcessor
                     "'" & "TO=" & eml.To & "CC=" & eml.Cc & "BCC=" & eml.Bcc & "', " & _
                     "'" & CType(IIf(eml.From.Length > 0, eml.From, "."), String) & "', " & _
                     "'" & CType(IIf(eml.Subject.Length > 0, eml.Subject, "."), String) & "', " & _
-                    "TO_DATE('" & System.DateTime.Now.ToString & "','MM/DD/YYYY HH:MI:SS AM') " & _
+                    "TO_DATE('" & System.DateTime.Now.ToString & "','MM/DD/YYYY HH:MI:SS AM'), '" & itmQuoted.EmployeeID & "' " & _
                 ")"
 
             rowsaffected = ExecNonQuery(strInsertQuery)
