44ebc87e093dbe265aa9ec140075bccf617e6864
240916Yury Arkadin <Yury.Arkadin@sdi.com>
2019-10-24 15:02:53 +0000

    ExpediterReload: new Oracle Helix project
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C8476

---
+++ ExpediterReload/ExpediterReload/ExpediterReload.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpediterReload1;
+using System.Net;
+
+namespace ExpediterReload
+{
+    class ExpediterReload
+    {
+        static void Main(string[] args)
+        {
+            var strResponse = "";
+            string processFlag = " ";
+
+            // InitializeLogger start here
+
+            
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            // Use SecurityProtocolType.Ssl3 if needed for compatibility reasons
+
+            Logger m_oLogger;
+            string sLogPath = Environment.CurrentDirectory;
+            if (!sLogPath.EndsWith(@"\"))
+                sLogPath += @"\";
+            sLogPath += "Logs";
+            m_oLogger = new Logger(sLogPath, "ExpediterReload");
+            m_oLogger.LogMessage("Main", "Started utility ExpediterReload");
+
+            //WMReceiptsMappingAPIAccess objWMReceiptsMappingAPIAccess = new WMReceiptsMappingAPIAccess();
+            //WMReceiptsMappingDAL objWMReceiptsMappingDAL = new WMReceiptsMappingDAL();
+            //strResponse = objWMReceiptsMappingAPIAccess.postWMReceiptMappingData(m_oLogger);
+            if (strResponse.ToUpper() == "SUCCESS")
+            {
+                processFlag = "I";
+            }
+            else
+            {
+                processFlag = "E"; //error
+            }
+            //objWMReceiptsMappingDAL.UpdateWMReceiptMappingData(m_oLogger, processFlag);
+
+            m_oLogger.LogMessage("Main", "ExpediterReload End");
+
+        }
+    }
+}
