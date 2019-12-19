1e29a0356c3a8bd90a467656c7fb54d90beb6759
vitaly.rovensky <vitaly.rovensky@isacorp.tfs.local>
2019-12-19 19:23:41 +0000

    Project Initial Check In
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C8681

---
+++ DymoPrintLabels/DymoPrintLabels/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports DYMO.Label.Framework
+
+
+Public Class Form1
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        Dim iNumOfLbls As Integer = 1
+        Dim myDymo As Object
+        Dim myLabel As Object
+        Dim sItemID As String = ""
+        Dim sDescr As String = ""
+        'Dim sItemID As String = ""
+
+        sItemID = "C100004334"
+        sDescr = "Some Label Description"
+
+        myDymo = CreateObject("Dymo.Dymoaddin")
+
+        myLabel = CreateObject("Dymo.Dymolabels")
+
+        myDymo.Open(CStr("C:\\Program Files\\Dymo Label\\Label Files\\WebPartner Label6(new).LWT"))
+
+        myLabel.SetField("ItemID_1", sItemID)
+
+        myLabel.SetField("Description", sDescr)
+
+        myDymo.Print(iNumOfLbls, True)
+
+    End Sub
+End Class
