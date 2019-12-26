e12e98a1048452f0d9af1a3c8e1cb158511807eb
240916Yury Arkadin <Yury.Arkadin@sdi.com>
2019-12-26 16:34:17 +0000

    Added code to send data to dymo printer
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C8694

--- WebPrintDymo/WebPrintDymo/WebForm1.aspx.vb
+++ WebPrintDymo/WebPrintDymo/WebForm1.aspx.vb
@@ -2,7 +2,28 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim iNumOfLbls As Integer = 1
+        Dim myDymo As Object
+        Dim myLabel As Object
+        Dim sItemID As String = ""
+        Dim sDescr As String = ""
+        'Dim sItemID As String = ""
 
+
+        sItemID = "C100004334"
+        sDescr = "Some Label Description"
+
+        myDymo = CreateObject("Dymo.Dymoaddin")
+
+        myLabel = CreateObject("Dymo.Dymolabels")
+
+        myDymo.Open(CStr("C:\\Program Files\\Dymo Label\\Label Files\\WebPartner Label6(new).LWT"))
+
+        myLabel.SetField("ItemID_1", sItemID)
+
+        myLabel.SetField("Description", sDescr)
+
+        myDymo.Print(iNumOfLbls, True)
     End Sub
 
 End Class
