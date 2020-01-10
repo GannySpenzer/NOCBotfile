c37144bdada58c079576a9ad3c10b5200467dc49
vitaly.rovensky <vitaly.rovensky@isacorp.tfs.local>
2020-01-10 19:46:57 +0000

    Commented out code in Page_Load
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C8753

--- WebPrintDymo/WebPrintDymo/WebForm1.aspx.vb
+++ WebPrintDymo/WebPrintDymo/WebForm1.aspx.vb
@@ -2,37 +2,37 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Dim iNumOfLbls As Integer = 1
-        Dim myDymo As Object
-        Dim myLabel As Object
-        Dim sItemID As String = ""
-        Dim sDescr As String = ""
+        'Dim iNumOfLbls As Integer = 1
+        'Dim myDymo As Object
+        'Dim myLabel As Object
         'Dim sItemID As String = ""
+        'Dim sDescr As String = ""
+        ''Dim sItemID As String = ""
 
-        Dim zz As New DYMO.DymoAddIn()
-        Dim yy As New Dymo.DymoLabels()
+        'Dim zz As New DYMO.DymoAddIn()
+        'Dim yy As New Dymo.DymoLabels()
 
-        yy.SetField("ItemID_1", sItemID)
-        yy.SetField("Description", sDescr)
-        zz.StartPrintJob()
-        zz.Print(1, True)
+        'yy.SetField("ItemID_1", sItemID)
+        'yy.SetField("Description", sDescr)
+        'zz.StartPrintJob()
+        'zz.Print(1, True)
 
 
-        'test new line here
-        sItemID = "C100004334"
-        sDescr = "Some Label Description"
+        ''test new line here
+        'sItemID = "C100004334"
+        'sDescr = "Some Label Description"
 
-        myDymo = CreateObject("Dymo.Dymoaddin")
+        'myDymo = CreateObject("Dymo.Dymoaddin")
 
-        myLabel = CreateObject("Dymo.Dymolabels")
+        'myLabel = CreateObject("Dymo.Dymolabels")
 
-        myDymo.Open(CStr("C:\\Program Files\\Dymo Label\\Label Files\\WebPartner Label6(new).LWT"))
+        'myDymo.Open(CStr("C:\\Program Files\\Dymo Label\\Label Files\\WebPartner Label6(new).LWT"))
 
-        myLabel.SetField("ItemID_1", sItemID)
+        'myLabel.SetField("ItemID_1", sItemID)
 
-        myLabel.SetField("Description", sDescr)
+        'myLabel.SetField("Description", sDescr)
 
-        myDymo.Print(iNumOfLbls, True)
+        'myDymo.Print(iNumOfLbls, True)
     End Sub
 
 End Class
