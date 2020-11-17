bd135038add216b18f97d29fc495ab74431b637b
AVACORP <AVACORP@sdi.com>
2020-11-17 22:14:54 +0000

    SDI - 19584 Solvay Phase I ( WMReceipts - OutboundService )
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C10741

--- WMReceiptsMapping/WMReceiptsMapping/WMReceiptsMapping.cs
+++ WMReceiptsMapping/WMReceiptsMapping/WMReceiptsMapping.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,7 @@ namespace WMReceiptsMapping
             // InitializeLogger start here
 
             Logger m_oLogger;
-            string sLogPath = Environment.CurrentDirectory;
+            string sLogPath = ConfigurationManager.AppSettings["LogFilePath"];
             if (!sLogPath.EndsWith(@"\"))
                 sLogPath += @"\";
             sLogPath += "Logs";
