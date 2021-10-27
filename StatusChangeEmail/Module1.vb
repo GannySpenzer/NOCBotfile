62090dd6c58f79d4030fc6ad0cdc0b6b4e4ffbe3
AVACORP <AVACORP@sdi.com>
2021-10-27 15:17:03 +0000

    SDI - 36315 Change in mobile notification[new query change]
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C13604

--- StatusChangeEmail/Module1.vb
+++ StatusChangeEmail/Module1.vb
@@ -2618,7 +2618,7 @@ Module Module1
         Try
             Dim NotificationContent As String = subject
             Dim _notificationResult As New DataSet
-            Dim notificationSQLStr = "SELECT DISTINCT(DEVICE_INFO) FROM SDIX_USER_TOKEN WHERE ISA_EMPLOYEE_ID = '" + Session_UserID + "' AND DEVICE_INFO IS NOT NULL AND LOWER(DEVICE_INFO) <> 'unknown unknown' AND LOWER(DEVICE_INFO)<>'webapp' order by last_update_dttm desc fetch first 1000 rows only"
+            Dim notificationSQLStr = "Select distinct(DEVICE_INFO) FROM (SELECT DISTINCT(DEVICE_INFO) FROM SDIX_USER_TOKEN WHERE ISA_EMPLOYEE_ID = '" + Session_UserID + "' AND DEVICE_INFO IS NOT NULL AND LOWER(DEVICE_INFO) <> 'unknown unknown' AND LOWER(DEVICE_INFO)<>'webapp' order by last_update_dttm desc fetch first 1000 rows only)"
             _notificationResult = ORDBAccess.GetAdapter(notificationSQLStr, connectOR)
             If _notificationResult.Tables.Count > 0 Then
                 If _notificationResult.Tables(0).Rows.Count > 0 Then
