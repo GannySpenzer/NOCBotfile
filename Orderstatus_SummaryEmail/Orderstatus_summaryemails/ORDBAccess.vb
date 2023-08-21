0a2cb920d64ac88c655d19cdda1f4c42a2ffdbda
AVACORP <AVACORP@sdi.com>
2023-08-21 20:02:34 +0000

    INC0025710-Orderstatus summary email
    git-tfs-id: [http://vsfoundations2:8080/tfs/insiteonline]$/InsiteOnline 3.1/ConsoleUtilities;C20359

---
+++ Orderstatus_SummaryEmail/Orderstatus_summaryemails/ORDBAccess.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Data.OleDb
+
+Public Class ORDBAccess
+    Public Shared Function GetAdapter(ByVal p_strQuery As String, ByVal connection As OleDbConnection) As DataSet
+
+        Dim UserdataSet As System.Data.DataSet = New System.Data.DataSet
+
+        Try
+            Dim Command As OleDbCommand = New OleDbCommand(p_strQuery, connection)
+            If connection.State = ConnectionState.Open Then
+                'do nothing
+            Else
+                connection.Open()
+            End If
+            Dim dataAdapter As OleDbDataAdapter =
+                    New OleDbDataAdapter(Command)
+
+            dataAdapter.Fill(UserdataSet)
+            connection.Close()
+        Catch objException As Exception
+            'MsgBox(objException.ToString, MsgBoxStyle.Critical)
+            Try
+                connection.Close()
+            Catch ex As Exception
+
+            End Try
+        End Try
+
+        Return UserdataSet
+
+    End Function
+    Public Shared Function GetScalar(ByVal p_strQuery As String, ByVal connection As OleDbConnection) As String
+        Try
+
+            Dim Command = New OleDbCommand(p_strQuery, connection)
+            'connection.Open()
+            Dim strReturn As String
+            strReturn = Command.ExecuteScalar()
+            'connection.Close()
+            Return strReturn
+        Catch objException As Exception
+            'MsgBox(objException.ToString, MsgBoxStyle.Critical)
+        End Try
+    End Function
+
+End Class
